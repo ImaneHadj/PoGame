7fcf6086d7f3646e9f7520055e88f55398712d8e
Imane Hadj <imane.hadj.abd@gmail.com>
2020-12-01 17:33:25 +0100

    rendu 3

---
+++ world.py
@@ -0,0 +1,13 @@
+import random
+
+from constants import *
+
+def create_world():
+    world = [[[], ["clé"], ["chaise"], ["porte"], ["lunettes"]],
+             [["ordinateur"],["bouteille"],["portable"],["arbre"],["voiture"]],
+             [["souris"],["chat"],[],[],["elephant"]],
+             [["chateau"],[],[],[],["imane"]],
+             [[],["word"],["princesse"],["jonny"],[]],
+             [["tableau"],[],["veste"],["masque"],["dragon"]]]
+
+    return world
